ca3ca6fc7e71b4bd7aa7b19da1b7ad895e11b74b
Bradley C Thuringer <bcthuringer@gmail.com>
2025-05-16 13:03:57 -0500

    Fix 403 error by updating S3 and CloudFront configuration

--- infrastructure/bin/blog-serverless.ts
+++ infrastructure/bin/blog-serverless.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { SimplifiedBlogStack } from '../templates/simplified-stack';
+import { FixedBlogStack } from '../templates/fixed-stack';
 
 const app = new cdk.App();
-new SimplifiedBlogStack(app, 'BlogServerlessStack', {
+new FixedBlogStack(app, 'BlogServerlessStack', {
   env: { 
     account: process.env.CDK_DEFAULT_ACCOUNT, 
     region: process.env.CDK_DEFAULT_REGION || 'us-east-1' 
---
+++ infrastructure/templates/fixed-stack.ts
@@ -0,0 +1,195 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as path from 'path';
+
+export class FixedBlogStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // S3 bucket for frontend hosting
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html',
+      publicReadAccess: true, // Allow public read access for website hosting
+      blockPublicAccess: new s3.BlockPublicAccess({
+        blockPublicAcls: false,
+        blockPublicPolicy: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false
+      }),
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // S3 bucket for blog media uploads
+    const mediaBucket = new s3.Bucket(this, 'MediaBucket', {
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      cors: [
+        {
+          allowedMethods: [s3.HttpMethods.GET, s3.HttpMethods.PUT, s3.HttpMethods.POST],
+          allowedOrigins: ['*'],
+          allowedHeaders: ['*'],
+        },
+      ],
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    // CloudFront Origin Access Identity for media bucket
+    const mediaOAI = new cloudfront.OriginAccessIdentity(this, 'MediaOAI', {
+      comment: 'OAI for accessing media bucket'
+    });
+
+    // Grant read access to the media bucket for CloudFront
+    mediaBucket.grantRead(mediaOAI);
+
+    // CloudFront distribution for website
+    const distribution = new cloudfront.Distribution(this, 'Distribution', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(websiteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD_OPTIONS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+      },
+      additionalBehaviors: {
+        '/media/*': {
+          origin: new origins.S3Origin(mediaBucket, {
+            originAccessIdentity: mediaOAI
+          }),
+          viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+      },
+      defaultRootObject: 'index.html',
+      errorResponses: [
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+      ],
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_100, // Use only North America and Europe
+    });
+
+    // Cognito User Pool for authentication
+    const userPool = new cognito.UserPool(this, 'UserPool', {
+      selfSignUpEnabled: true,
+      autoVerify: { email: true },
+      signInAliases: { email: true },
+      standardAttributes: {
+        email: { required: true, mutable: true },
+        fullname: { required: true, mutable: true },
+      },
+      passwordPolicy: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireDigits: true,
+        requireSymbols: true,
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+    });
+
+    // Cognito User Pool Client
+    const userPoolClient = new cognito.UserPoolClient(this, 'UserPoolClient', {
+      userPool,
+      authFlows: {
+        userPassword: true,
+        userSrp: true,
+      },
+      oAuth: {
+        flows: {
+          authorizationCodeGrant: true,
+        },
+        scopes: [cognito.OAuthScope.EMAIL, cognito.OAuthScope.OPENID, cognito.OAuthScope.PROFILE],
+        callbackUrls: ['http://localhost:3000/callback', `https://${distribution.distributionDomainName}/callback`],
+        logoutUrls: ['http://localhost:3000/', `https://${distribution.distributionDomainName}/`],
+      },
+    });
+
+    // DynamoDB Tables
+    const usersTable = new dynamodb.Table(this, 'UsersTable', {
+      partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    const blogsTable = new dynamodb.Table(this, 'BlogsTable', {
+      partitionKey: { name: 'blogId', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'createdAt', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    blogsTable.addGlobalSecondaryIndex({
+      indexName: 'userIdIndex',
+      partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'createdAt', type: dynamodb.AttributeType.STRING },
+      projectionType: dynamodb.ProjectionType.ALL,
+    });
+
+    // Store configuration values in SSM Parameter Store
+    new ssm.StringParameter(this, 'UserPoolIdParameter', {
+      parameterName: '/blog/auth/userPoolId',
+      stringValue: userPool.userPoolId,
+      description: 'Cognito User Pool ID for the blog application',
+    });
+
+    new ssm.StringParameter(this, 'UserPoolClientIdParameter', {
+      parameterName: '/blog/auth/userPoolClientId',
+      stringValue: userPoolClient.userPoolClientId,
+      description: 'Cognito User Pool Client ID for the blog application',
+    });
+
+    new ssm.StringParameter(this, 'WebsiteBucketParameter', {
+      parameterName: '/blog/storage/websiteBucket',
+      stringValue: websiteBucket.bucketName,
+      description: 'S3 bucket name for website hosting',
+    });
+
+    new ssm.StringParameter(this, 'MediaBucketParameter', {
+      parameterName: '/blog/storage/mediaBucket',
+      stringValue: mediaBucket.bucketName,
+      description: 'S3 bucket name for media storage',
+    });
+
+    new ssm.StringParameter(this, 'CloudFrontDomainParameter', {
+      parameterName: '/blog/distribution/domain',
+      stringValue: distribution.distributionDomainName,
+      description: 'CloudFront distribution domain name',
+    });
+
+    // Outputs
+    new cdk.CfnOutput(this, 'WebsiteBucketName', {
+      value: websiteBucket.bucketName,
+    });
+
+    new cdk.CfnOutput(this, 'MediaBucketName', {
+      value: mediaBucket.bucketName,
+    });
+
+    new cdk.CfnOutput(this, 'DistributionDomainName', {
+      value: distribution.distributionDomainName,
+    });
+
+    new cdk.CfnOutput(this, 'UserPoolId', {
+      value: userPool.userPoolId,
+    });
+
+    new cdk.CfnOutput(this, 'UserPoolClientId', {
+      value: userPoolClient.userPoolClientId,
+    });
+  }
+}
